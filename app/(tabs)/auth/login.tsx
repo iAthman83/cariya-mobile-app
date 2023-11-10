798625d02111175e00426ae25cc2c5c9b47e8c87
Abubakar Athman <athman.abu@gmail.com>
2023-11-10 16:49:26 +0300

    auth screen

---
+++ app/(tabs)/auth/login.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet, SafeAreaView } from "react-native";
+import { View, Text } from "../../../components/Themed";
+import MainHeader from "../../../components/utils/MainHeader";
+
+type Props = {};
+
+const login = (props: Props) => {
+  return (
+    // <SafeAreaView>
+    <View style={styles.container}>
+      <View>
+        <MainHeader input="welcome back!" />
+        <Text>Please sign in to your account</Text>
+      </View>
+      {/* Input boxes */}
+      <View>
+        <MainHeader input="welcome back!" />
+        <Text>Please sign in to your account</Text>
+      </View>
+      {/* different */}
+      <View>
+        <MainHeader input="welcome back!" />
+        <Text>Please sign in to your account</Text>
+      </View>
+    </View>
+    // </SafeAreaView>
+  );
+};
+
+export default login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+
+    // height: "100%",
+  },
+});
